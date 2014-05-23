59af824ba4b61e1f979a70f7d8ecf66d81626bcd
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2014-05-23 01:09:55 -0400

    Changed the rest endpoint

--- src/main/java/mvctest/web/RestHotelController.java
+++ src/main/java/mvctest/web/RestHotelController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hotels/rest")
+@RequestMapping("/rest/hotels")
 public class RestHotelController {
 	private HotelRepository hotelRepository;
 	
